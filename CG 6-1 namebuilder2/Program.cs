e56526c87a126ebc2772691bac6d0d06a6fee759
Kristen Shea <kstar61@live.com>
2018-08-14 21:10:54 -0500

    *

--- CG 6-1 namebuilder2/Program.cs
+++ CG 6-1 namebuilder2/Program.cs
@@ -6,7 +6,25 @@ namespace CG_6_1_namebuilder2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            Console.Write("Please type your first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Please type your last name: ");
+            string lastName = Console.ReadLine();
+
+
+
+            Console.ReadLine();
         }
+
+        //private static string FullName(firstName, lastName)
+        //{
+            
+
+
+        //}
+
+
+
     }
 }
